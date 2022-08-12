8e267ed29f3e3ddde8758a0109572fa80b7aeda1
cassiohtx <cassiohtx@gmail.com>
2022-08-12 01:48:46 -0300

    Realizada correção da tarefa 12 [Edição - Devolvido]

--- Controllers/EmprestimoController.cs
+++ Controllers/EmprestimoController.cs
@@ -52,7 +52,7 @@ namespace Biblioteca.Controllers
 
         public IActionResult Edicao(int id)
         {
-            Autenticacao.CheckLogin(this);
+            
             LivroService livroService = new LivroService();
             EmprestimoService em = new EmprestimoService();
             Emprestimo e = em.ObterPorId(id);
--- Models/EmprestimoService.cs
+++ Models/EmprestimoService.cs
@@ -10,11 +10,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Biblioteca.Models
 {
-    public class EmprestimoService 
+    public class EmprestimoService
     {
         public void Inserir(Emprestimo e)
         {
-            using(BibliotecaContext bc = new BibliotecaContext())
+            using (BibliotecaContext bc = new BibliotecaContext())
             {
                 bc.Emprestimos.Add(e);
                 bc.SaveChanges();
@@ -23,7 +23,7 @@ namespace Biblioteca.Models
 
         public void Atualizar(Emprestimo e)
         {
-            using(BibliotecaContext bc = new BibliotecaContext())
+            using (BibliotecaContext bc = new BibliotecaContext())
             {
                 Emprestimo emprestimo = bc.Emprestimos.Find(e.Id);
                 emprestimo.NomeUsuario = e.NomeUsuario;
@@ -31,34 +31,34 @@ namespace Biblioteca.Models
                 emprestimo.LivroId = e.LivroId;
                 emprestimo.DataEmprestimo = e.DataEmprestimo;
                 emprestimo.DataDevolucao = e.DataDevolucao;
-                emprestimo.Devolvido = e.Devolvido; 
-                 bc.SaveChanges();
-                 //
+                emprestimo.Devolvido = e.Devolvido;
+                bc.SaveChanges();
+                
             }
         }
 
         public ICollection<Emprestimo> ListarTodos(FiltrosEmprestimos filtro)
         {
-            using(BibliotecaContext bc = new BibliotecaContext())
+            using (BibliotecaContext bc = new BibliotecaContext())
             {
                 IQueryable<Emprestimo> query;
-                
-                if(filtro != null)
+
+                if (filtro != null)
                 {
                     //definindo dinamicamente a filtragem
-                    switch(filtro.TipoFiltro)
+                    switch (filtro.TipoFiltro)
                     {
                         case "Usuario":
                             query = bc.Emprestimos.Where(e => e.NomeUsuario.Contains(filtro.Filtro));
-                        break;
+                            break;
 
                         case "Livro":
                             query = bc.Emprestimos.Where(e => e.Livro.Titulo.Contains(filtro.Filtro));
-                        break;
+                            break;
 
                         default:
                             query = bc.Emprestimos;
-                        break;
+                            break;
                     }
                 }
                 else
@@ -70,12 +70,12 @@ namespace Biblioteca.Models
                 // return bc.Emprestimos.Include(e => e.Livro).ToList();
             }
 
-            
+
         }
 
         public Emprestimo ObterPorId(int id)
         {
-            using(BibliotecaContext bc = new BibliotecaContext())
+            using (BibliotecaContext bc = new BibliotecaContext())
             {
                 return bc.Emprestimos.Find(id);
             }
